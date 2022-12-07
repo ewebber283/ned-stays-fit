be76ba6105c773f261604b274c9b5416ad9c1e43
Eric Webber <ewebber283@gmail.com>
2022-12-07 13:58:07 -0600

    finished about

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,7 +7,16 @@ function Home() {
             <div className='about'>
                 <h2>About Me</h2>
                 <div className='description'>
-                    <p> Aspiring software developer with a passion for learning and creating</p>
+                    <p> I am in my early 60s and I have been exercising in one form or another since I was 11 or 12 years old. My
+dad, a former college athlete and a really smart guy in general, instilled in me the concept that a strong
+mind needs a healthy body to support it.
+In addition to my dad, I was influenced along the way by Jack LaLanne and many other older athletes. I
+was always inspired by men and woman who seemed to age with strength. traing
+I was able to translate my passion for fitness into a nearly 40-year career as a physical therapist and
+personal trainer. Although I am retired, I am still very dedicated to staying fit and aging gracefully.
+I love to stay active and enjoy sailing, paddle-boarding, golfing and traveling in addition to cooking and
+being an avid reader and crossword puzzler. But, above all, I love spending time with my family and
+friends!</p>
                 </div> 
             </div>
             <div className='why'>
